327807c164a1afba36d3bd2968ea7e11a6c349b2
Sarmad Sajjad <100189495+Sarmad24x@users.noreply.github.com>
2026-02-22 16:22:59 +0500

    Update Code: Firebase Initialization

--- mehr_study_point/lib/main.dart
+++ mehr_study_point/lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,6 +8,7 @@ import 'providers/theme_provider.dart';
 import 'screens/splash_screen.dart';
 import 'theme/app_theme.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'firebase_options.dart';
 
 import 'widgets/auth_gate.dart';
 
@@ -30,7 +32,13 @@ Future<void> main() async {
 
 Future<void> _initializeFirebase() async {
   try {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    FirebaseFirestore.instance.settings = const Settings(
+      persistenceEnabled: true,
+      cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED,
+    );
     debugPrint("Firebase initialized successfully");
   } catch (e) {
     debugPrint("Firebase initialization failed: $e");
